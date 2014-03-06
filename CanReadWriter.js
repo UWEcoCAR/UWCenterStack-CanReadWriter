3c9deb27471a800a1dac93900ddc1be3508bfd79
Vinod <vinodr@uw.edu>
2014-03-06 04:19:26 -0500

    Fixed naming, and made CanReadWriter extend EventEmitter

--- CanReadWriter.js
+++ CanReadWriter.js
@@ -1,25 +1,16 @@
 var canReadWriter = require('./build/Release/canReadWriter');
 var events = require('events');
+var util = require('util');
 var eventsEmitter = new events.EventEmitter();
 
 var CanReadWriter = module.exports = function() {
     // init stuff
 };
 
+util.inherits(CanReadWriter, events.EventEmitter);
+
 // will run the initialization in C++. takes in the callback to be run by C++ once
 // a meaningful message is found
-CanReadWriter.prototype.init = function(func) {
+CanReadWriter.prototype.Init = function(func) {
   return canReadWriter.readResult(func);
-}
-
-// executes the callbacks for the given signal with a parameter 
-// that is the given value
-CanReadWriter.prototype.executeReadCallbacks = function(signalName, value) {
-  eventsEmitter.emit(signalName, value);
-}
-
-
-// will register the callback for the given signal to be ran later
-CanReadWriter.prototype.registerReadCallback = function(signalName, callback) {
-  eventsEmitter.on(signalName, callback);
 }
--- test.js
+++ test.js
@@ -5,10 +5,16 @@ var CanReadWriter = require('./CanReadWriter');
 var canReadWriter = new CanReadWriter();
 
 // registering 2 callbacks for a "voltage" signal
-canReadWriter.registerReadCallback("voltage", function(a) { console.log(a); });
-canReadWriter.registerReadCallback("voltage", function(b) { console.log(b+1); });
+canReadWriter.on("voltage", function(a) { console.log(a); });
+canReadWriter.on("voltage", function(b) { console.log(b+1); });
 
 // initialize with the given callback that will execute all listeners for the
 // "voltage" event. This will output 5 and then 6 in the console.
-canReadWriter.init(canReadWriter.executeReadCallbacks);
+
+// TODO: was unable to just replace canReadWriter.emit for _Emit. Needed a wrapper
+//       function. Will continue to look into this.
+var _Emit = function(a, b) {
+  canReadWriter.emit(a, b);
+}
+canReadWriter.Init(_Emit);
 
