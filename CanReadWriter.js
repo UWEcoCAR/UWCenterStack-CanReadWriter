c453cbb89ec3bd91414ea143a1be88ac0f664f3f
Mitchell Loeppky <mitchell@loeppky.com>
2014-04-18 13:41:29 -0700

    Added mail boxing

--- CanReadWriter.js
+++ CanReadWriter.js
@@ -4,11 +4,16 @@ var util = require('util');
 
 var CanReadWriter = module.exports = function() {
 	var self = this;
+    this._mailbox = {};
     return canReadWriter.start(function(name, value) {
-		 self.emit(name, value);
+        mailbox[name] = value;
+		self.emit(name, value);
 	});
 };
 
 util.inherits(CanReadWriter, events.EventEmitter);
 
 CanReadWriter.prototype.write = canReadWriter.write;
+CanReadWriter.prototype.getMail = function(address) {
+    return this._mailbox[address];
+};
